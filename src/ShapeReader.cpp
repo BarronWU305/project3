def2bee2aa977af935245804958ee22f61fc1b85
kelllllllly <kellyveintimilla1@gmail.com>
2023-11-07 15:00:31 -0500

    added to cpp

--- src/ShapeReader.cpp
+++ src/ShapeReader.cpp
@@ -1,1 +1,18 @@
 #include <ShapeReader.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+ShapeReader::readFile(string filename, ShapeQueue &queue){
+
+
+//open read in file
+//switch
+// add to queue 
+
+}
+
+// switch
+// read in file , make shape, add to queue
