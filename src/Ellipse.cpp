dcd052221157609a1c1a918341ac0218a09895ed
Taylor <tsanders3515@gmail.com>
2023-11-07 15:00:21 -0500

    beginning to work on Ellipse.cpp

---
+++ src/Ellipse.cpp
@@ -0,0 +1,28 @@
+#include <Ellipse.h>
+#include <iostream>
+
+using namespace std;
+
+//get axis A
+double Ellipse::getFirstAxis(){
+}
+
+//get axis B
+double Ellipse::getSecondAxis(){
+
+}
+
+//get perimeter
+double Ellipse::getPerimeter(double axis_first_, double axis_second_){
+
+}
+
+//get area
+double Ellipse::getArea(double axis_first_, double axis_second_){
+
+}
+
+//print Ellipse
+void Ellipse::printEllipse(){
+
+}
