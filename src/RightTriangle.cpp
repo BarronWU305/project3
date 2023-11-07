2cbaeaee007b1f59c2c2ef607b939a8868e429d0
S. Stephen Adair <adairs2@mailbox.winthrop.edu>
2023-11-07 14:34:36 -0500

    defined triangle cpp file

---
+++ src/RightTriangle.cpp
@@ -0,0 +1,18 @@
+#include <RightTriangle.h>
+
+
+
+void RightTriangle::SetBase(double Base){
+    base_ = Base;
+}
+double RightTriangle::GetBase(){
+    return base_;
+}
+
+
+void RightTriangle::SetHeight(double Height){
+    height_ = Height;
+}
+double RightTriangle::GetHeight(){
+    return height_;
+}
