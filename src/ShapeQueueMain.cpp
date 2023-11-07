a92dc88dc78598984cee65eef9c98944e059c5da
Ansely Barron <barrona5@winthrop.edu>
2023-11-07 14:51:11 -0500

    added vars, comment, and a way to open file

--- src/ShapeQueueMain.cpp
+++ src/ShapeQueueMain.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-#include <Circle.h>
+#include <fstream> //ADDED IN FSTREAM IN CASE FILES NEED READING IN
+#include <Circle.h>//HEADERFILES HERE
 #include <Hexagon.h>
 #include <Ellipse.h>
 #include <Node.h>
@@ -14,15 +15,30 @@
 using namespace std; 
 
 int main() {
-
-    string thingy ; 
+//VARS NEEDED
+    string fileName;
+ 
     
 
+//GET FILE NAME WE NEED TO BE READ IN  
+	cout << "Enter out the name of the file that we will be reading in << endl; 
+	cin >> fileName; 
+
+//FOR INPUT ERRORS WHEN FILE OPEN 
+	ifstream inputFile(fileName); 
+	if(!inputfile)
+	{ 
+		cout << "ERROR: the file cannot be opened. Try again" << endl; 
+	}
 
 
 
 
 
+//CLOSES FILE AT THE END
+	inputFile.close(); 
 
+
+//All is good :)
     return 0; 
 }
