8fb338a802cb57d9ee341cdfd6588857dab167b6
K. Goff <goffk3>
2023-11-07 14:26:35 -0500

    finished header file

---
+++ inc/Rectangle.h
@@ -0,0 +1,28 @@
+#ifndef RECTANGLE_
+#define RECTANGLE_
+
+class rectangle{
+
+private:
+double length;
+double width;
+
+
+
+
+
+public:
+double getLength() const;
+double getWidth() const;
+
+
+
+
+
+
+};
+
+
+
+
+#endif
