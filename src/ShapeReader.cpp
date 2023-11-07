845f264a529269b43236a39d3e4552f3ee3d5674
kelllllllly <kellyveintimilla1@gmail.com>
2023-11-07 15:15:16 -0500

    added to ShapeReader.cpp

--- src/ShapeReader.cpp
+++ src/ShapeReader.cpp
@@ -6,6 +6,18 @@ using namespace std;
 
 
 void ShapeReader::readFile(string filename, ShapeQueue &queue){
+    int returnCode = 0;
+
+    ifstream fin; 
+    fin.open(filename);
+
+    if (!fin.is_open()){
+        
+        cerr << "ERROR:" << filename << " does not exist." << endl;
+        returnCode =1;
+    }
+
+    
 
 
 //open read in file
