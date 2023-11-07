a1ab8ec3df1915361b9a5730624c4f548b75968c
kelllllllly <kellyveintimilla1@gmail.com>
2023-11-07 15:34:16 -0500

    changed  lines 4-9

--- inc/ShapeReader.h
+++ inc/ShapeReader.h
@@ -1,21 +1,19 @@
 #ifndef SHAPEREADER_H_
 #define SHAPEREADER_H_
 
-#include "ShapeQueue.h"
-#include "Circle.h"
-#include "Ellipse.h"
-#include "Hexagon.h"
-#include "Rectangle.h"
-#include "RightTriangle.h"
-
-#include <iostream>
-#include <fstream>
+#include <ShapeQueue.h>
+#include <Circle.h>
+#include <Ellipse.h>
+#include <Hexagon.h>
+#include <Rectangle.h>
+#include <RightTriangle.h>
 
 using namespace std;
 
 class ShapeReader {
 public: 
-    void readFile(string filename, ShapeQueue);
+  //  ShapeReader(); 
+    void readFile(string filename, ShapeQueue &queue);
 
 };
 
