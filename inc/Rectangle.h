669adee0043c6c9964886c00a10d801ba500c3df
R. Paul Wiegand <rpwiegand@gmail.com>
2023-11-09 12:55:28 -0500

    Dr. Wiegand being obnoxious

--- inc/Rectangle.h
+++ inc/Rectangle.h
@@ -1,22 +1,25 @@
 #ifndef RECTANGLE_
 #define RECTANGLE_
 
-class Rectangle {
+#include <Shape.h>
 
-private:
-  double length_;
-  double width_;
-
+class Rectangle : public Shape {
 public:
+  Rectangle(double Length, double Width);
+
   void setLength (double Length);
   double getLength() const;
   
   void setWidth (double Width);
   double getWidth() const;
 
-  double getArea() const;
-  double getPerimeter () const; //get the perimeter on thursday 
+  virtual double getArea() const;
+  virtual double getPerimeter () const; //get the perimeter on thursday 
+  virtual void print() const;
 
+protected:
+  double length_;
+  double width_;
 
 
 };
--- src/Rectangle.cpp
+++ src/Rectangle.cpp
@@ -1,26 +1,30 @@
 #include<iostream>
 #include<cmath>
 #include<iomanip>
-#include"../inc/Rectangle.h"
+#include<Rectangle.h>
 
+Rectangle::Rectangle(double Length, double Width) 
+   : Shape("Rectangle"), length_(Length), width_(Width) {} 
 
 void Rectangle::setLength(double Length){
     length_= Length;
 }
-double Rectangle:: getLength() const{
+double Rectangle::getLength() const{
     return length_;
 
 }
 void Rectangle::setWidth(double Width){
     width_= Width;
 }
-double Rectangle:: getWidth() const{
+double Rectangle::getWidth() const{
     return width_;
 }
 
-double Rectangle:: getArea() const{
+double Rectangle::getArea() const{
     return (width_ * length_);
 }
+
+void Rectangle::print() const {}
 
 
 
