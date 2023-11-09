7fcda5b95c0550a5e37e7a2ee923350ccf39f3d6
Home Desktop <auggie.sabana@gmail.com>
2023-11-09 00:58:02 -0500

    Played around with some stuff. Check in the mornin

--- src/ShapeQueueMain.cpp
+++ src/ShapeQueueMain.cpp
@@ -17,26 +17,25 @@ using namespace std;
 int main() {
 //VARS NEEDED
     string fileName;
- 
-    
+	ShapeQueue Queue; 
+    int codeReturn; // BC IT'S RETURNING AN INT, FIGURE WE USE IT HERE 
 
 //GET FILE NAME WE NEED TO BE READ IN  
 	cout << "Enter out the name of the file that we will be reading in" << endl; 
 	cin >> fileName; 
 
-//FOR INPUT ERRORS WHEN FILE OPEN 
-	ifstream inputFile(fileName); 
-	if(!inputfile)
-	{ 
-		cout << "ERROR: the file cannot be opened. Try again" << endl; 
+// FIGURE WE CAN JUST CALL TO READ IT FROM THE READER FILE WE HAVE 
+	codeReturn = readFile(fileName, Queue); // CALL IT HERE AND SET IT TO THE VAR
+
+// AND PRINT OUT THE QUEUE SINCE IT'S ALREADY CALLING THE PRINT FUNC FOR THE SHAPES 
+	if(codeReturn == 0) {
+		cout << "Shape in our Queue: " << endl; 
+		Queue.printQueue(); 
 	}
-
-
-
-
-
-//CLOSES FILE AT THE END
-	inputFile.close(); 
+// FALLBACK OFC 
+	else {
+		cout << "Failed to read the shapes T_T" << endl; 
+	}
 
 
 //All is good :)
