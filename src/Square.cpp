1c0328f9b0b16ec75e28e6339757e453a94abed8
Kevin Justin <kevin.justin04@gmx.com>
2023-11-09 14:19:44 -0500

    somewhat finished

--- src/Square.cpp
+++ src/Square.cpp
@@ -4,26 +4,22 @@ Square::Square(double Side) : Rectangle(Side, Side) {
   name_ = "Square";
 }
 
-virtual void Square::setLength (double Length) {
+void Square::setLength (double Length) {
   Rectangle::setLength(Length);
   Rectangle::setWidth(Length);
 }
 
-<<<<<<< HEAD
-virtual double Square::getArea(double Length) const {
+void Square::setWidth (double Width) {
+  Rectangle::setLength(Width);
+  Rectangle::setWidth(Width);
+}
+
+double Square::getArea(double Length) const {
   double area = Length * Length;
   return area;
 }
 
-virtual double Square::getPerimeter() const {
+double Square::getPerimeter(double Length) const {
   double perimeter = Length * 4;
   return perimeter;
 }
-=======
-  void Square::setWidth (double Width) {
-
-
-    
-  }
- // Also handle setWidth
->>>>>>> refs/remotes/origin/main
