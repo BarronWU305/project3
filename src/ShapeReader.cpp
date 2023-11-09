06f6fa1266b073ee009b3013dcfde2cfb86509b8
nataliesanquintin <sanquintinn2@winthrop.edu>
2023-11-09 14:02:22 -0500

    cleaning up

--- src/ShapeReader.cpp
+++ src/ShapeReader.cpp
@@ -5,117 +5,95 @@
 
 using namespace std;
 
-int readFile(string filename, ShapeQueue &queue)
-{
+int readFile(string filename, ShapeQueue &queue) {
      int returnCode = 0;
 
      ifstream fin;
      fin.open(filename);
 
-     if (!fin.is_open())
-     {
+     if (!fin.is_open()) {
           cerr << "ERROR:" << filename << " does not exist." << endl;
           returnCode = 1;
      }
-     else
-     {
-          while (!fin.eof())
-          {
+     else {
+          while (!fin.eof()) {
                string shapeName;
                fin >> shapeName;
 
-               if (shapeName == "Circle")
-               {
+               if (shapeName == "Circle") {
                     double radius;
                     fin >> radius;
                     // Check if reading failed
-                    if (fin.fail())
-                    {
+                    if (fin.fail()) {
                          cerr << "ERROR: Failed to read radius." << endl;
                     }
-                    else
-                    {
+                    else {
                          Shape *ptr = new Circle(radius); // Except the Circle.h doesn't show this constructor
                          queue.pushShape(ptr);
                     }
                }
-               else if (shapeName == "Square")
-               {
+               else if (shapeName == "Square") {
                     double length;
                     double width;
                     fin >> length >> width;
                     // Check if reading failed
-                    if (fin.fail())
-                    {
+                    if (fin.fail()) {
                          cerr << "ERROR: Failed to read length or width." << endl;
                     }
-                    else
-                    {
+                    else {
                          Shape *ptr = new Square(length, width); // Except the Square.h doesn't show this constructor
                          queue.pushShape(ptr);
                     }
                }
-               else if (shapeName == "Ellipse")
-               {
+               else if (shapeName == "Ellipse") {
                     double firstAxis;
                     double secondAxis;
                     fin >> firstAxis >> secondAxis;
                     // Check if reading failed
-                    if (fin.fail())
-                    {
+                    if (fin.fail()) {
                          cerr << "ERROR: Failed to read first and second Axis." << endl;
                     }
-                    else
-                    {
+                    else {
                          Shape *ptr = new Ellipse(firstAxis, secondAxis); // Except the Ellipse.h doesn't show this constructor
                          queue.pushShape(ptr);
                     }
                }
-               else if (shapeName == "Hexagon")
-               {
+               else if (shapeName == "Hexagon") {
                     int sideLength;
                     fin >> sideLength;
                     // Check if reading failed
-                    if (fin.fail())
-                    {
+                    if (fin.fail()) {
                          cerr << "ERROR: Failed to read side length." << endl;
                     }
-                    else
-                    {
+                    else {
                          Shape *ptr = new Hexagon(sideLength); // Except the Hexagon.h doesn't show this constructor
                          queue.pushShape(ptr);
                     }
                }
 
-               else if (shapeName == "Rectangle")
-               {
+               else if (shapeName == "Rectangle") {
                     double length;
                     double width;
                     fin >> length >> width;
                     // Check if reading failed
-                    if (fin.fail())
-                    {
+                    if (fin.fail()) {
                          cerr << "ERROR: Failed to read length or width." << endl;
                     }
-                    else
-                    {
+                    else {
                          Shape *ptr = new Rectangle(length, width); // Except the Rectangle.h doesn't show this constructor
                          queue.pushShape(ptr);
                     }
                }
 
-               else if (shapeName == "RightTriangle")
-               {
+               else if (shapeName == "RightTriangle") {
                     double base;
                     double height;
                     fin >> base >> height;
                     // Check if reading failed
-                    if (fin.fail())
-                    {
+                    if (fin.fail()) {
                          cerr << "ERROR: Failed to read base or height." << endl;
                     }
-                    else
-                    {
+                    else {
                          Shape *ptr = new RightTriangle(base, height); // Except the RIghtTriangle.h doesn't show this constructor
                          queue.pushShape(ptr);
                     }
