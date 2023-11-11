8e8f441635ad12568c59a226ea2a5c67b10f7e43
ZachPapanicolas <142852243+ZachPapanicolas@users.noreply.github.com>
2023-11-11 03:34:08 -0500

    Update Hexagon.cpp

--- src/Hexagon.cpp
+++ src/Hexagon.cpp
@@ -1,20 +1,23 @@
 #include<iostream>
 #include<iomanip>
-#include<Hexagon.h>
+#include<Hexagon.h> // Include the header file for the hexagon class
 #include<cmath>
-
+ //Constructor for the Hexagon
 Hexagon::Hexagon(double Length) 
   : Shape("Hexagon"), length_(Length) {}
-
+//  Function to get the length 
 double Hexagon::getLength() const {
   return length_;
 }
+// Function to calculate the area of a the Hexagon
 double Hexagon::getArea() const {
   return (((3 * sqrt(3)) / 2) * length_);
 }
+// Function to calculate the perimeter
 double Hexagon::getPerimeter() const {
   return (length_ * 6);
 }
+// Print information about the hexagon
 void Hexagon::print() const {
   cout << "Shape: " << getName() << endl;
   cout << "Length = " << getLength() << endl;
