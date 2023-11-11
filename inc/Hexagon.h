69ecc3db5389ec4dfe2d3b838f9ef92c60243e5f
ZachPapanicolas <142852243+ZachPapanicolas@users.noreply.github.com>
2023-11-11 03:27:40 -0500

    Update Hexagon.h

--- inc/Hexagon.h
+++ inc/Hexagon.h
@@ -1,21 +1,26 @@
 #ifndef HEXAGON_H_
 #define HEXAGON_H_
 
-#include<Shape.h>
+#include<Shape.h> //include the header file
+
 
 using namespace std;
 
+
+//Hexagon class inherits from the Shape class
 class Hexagon : public Shape {
 public:
+  // Constructor that takes the length of the hexagon as a parameter
   Hexagon(double Length);
 
+// Accessor Functions
   double getLength() const;
   virtual double getArea() const;
   virtual double getPerimeter() const;
   virtual void print() const;
 
 protected:
-  double length_;
+  double length_; // Length of the hexagon
 };
 
 #endif
